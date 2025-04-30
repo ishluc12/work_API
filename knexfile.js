8d94aca09c667506a19f911f77ab04995ed22875
ishluc12 <jlucishimwe2003@gmail.com>
2025-04-30 23:08:54 +0200

    second commit

--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,7 @@ module.exports = {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      port: process.env.DB_PORT || 5432,
+      port: parseInt(process.env.DB_PORT) || 5432,
       ssl: { rejectUnauthorized: false } // For Render free tier
     },
     migrations: {
